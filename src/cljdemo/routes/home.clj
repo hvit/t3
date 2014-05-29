6c6a31d10197c3078ee1cb21a2473aba806b955e
weipan789 <weipan789@163.com>
2014-05-29 20:19:49 +0800

    first commit

---
+++ src/cljdemo/routes/home.clj
@@ -0,0 +1,15 @@
+(ns cljdemo.routes.home
+  (:use compojure.core)
+  (:require [cljdemo.layout :as layout]
+            [cljdemo.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
